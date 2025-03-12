5e6c065d1ea25dfcd205fa72621e46fdb2cd44b1
iton <swittah893@gmail.com>
2025-03-12 21:27:47 +0700

    docs

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@
 
     const schoolID  = "39xxx"           // school ID card (5 digits)
     const citizenID = "15xxxxxxxxxxx"   // citizen ID card (13 digits)
-    const type      = "sdq"             // teacher | sdq | atitude
+    const type      = "teacher"         // teacher | sdq | atitude
     const delay     = 500               // depends on internet connection (ms)
           
 /*  ==========================================================================
@@ -23,30 +23,27 @@
 ;   5. Type: git clone https://github.com/itonasd/auto-evaluation-tool
 ;   6. Type: cd auto-evaluation-tool
 ;   7. Type: `npm i puppeteer`
-;   8. Type: `node ./src/main`
+;   8. Goto: ./src/main.js right click on it, click on edit
+;   9. After done inserting information, press Ctrl+S to save
+;   10. Type: `node ./src/main`
+;
+;   To execute again, repeat from 8 to 10 
+; 
+;
+; TROBLESHOOTING
+;   git | npm is not recognized as a name of a cmdlet
+;       -> try close and re-open powershell first, If the problem is not resolved, follow instruction below
+;       -> add git | npm to PATH https://www.eukhost.com/kb/how-to-add-to-the-path-on-windows-10-and-windows-11/
+;       -> in the “edit environment variable”, click “new” type <path/to/git | npm/folder>
+; 
+;   randomly error while executing
+;       -> might be due to a slow internet connection, try increasing delay parameter above
+;       -> if the problem is not resolved, browser might alert something while executing
+;          and interrupts the execution, there is still no fix for this case
 ; 
 ; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
+; android and ios are not supported
+; this project was developed in just 2 days
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */ 
    
 import { EvaluateConfig } from "./preprocessor.js";
